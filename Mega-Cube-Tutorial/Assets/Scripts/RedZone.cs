c552ec7f7ca81b99ea7947b8f31877edf99392b9
Safakylmz <safakyilmaz8@hotmail.com>
2022-09-08 00:03:39 +0300

    gameover panel
    restart button

--- Mega-Cube-Tutorial/Assets/Scripts/RedZone.cs
+++ Mega-Cube-Tutorial/Assets/Scripts/RedZone.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RedZone : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
     private void OnTriggerStay(Collider other)
     {
         Cube cube = other.GetComponent<Cube>();
@@ -12,7 +19,14 @@ public class RedZone : MonoBehaviour
             if(!cube.isMainCube && cube.cubeRigidbody.velocity.magnitude < 0.1f)
             {
                 Debug.Log("Game Over");
+                gameOverPanel.SetActive(true);
+                GameObject.Find("Touch Slider").GetComponent<TouchSlider>().enabled = false; // touch slider scripti deaktif oluyor.
             }
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
